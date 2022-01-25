da9e72adfac1084b7bd3c90a3c12c687bfde8279
Zhi Li <lzblack@users.noreply.github.com>
2022-01-25 18:35:44 -0500

    Create hello_01.py

---
+++ session03/hello_01.py
@@ -0,0 +1,47 @@
+# name = input("Please enter your name<< ")
+
+# print('Hello,', name)
+# print(f'Hello, {name}!')
+
+print('Naah, na na nanana naah, nanana naah, hey Jude.\n' * 10)
+
+pi = 13.1415926
+
+print(f'Pi equals {pi:.6f}.')
+print(f'Pi equals {pi:8.5f}.')
+print(f'Pi equals {pi:8.2f}.')
+print()
+
+a = 15
+
+# binary
+print(f'{a:b}')
+
+# hexadecimal
+print(f"{a:x}")
+
+# octal
+print(f"{a:o}")
+
+# scientific
+print(f"{a:e}")
+
+s1 = 'a'
+s2 = 'ab'
+s3 = 'abc'
+s4 = 'abcd'
+
+print(f'{s1:<10}')
+print(f'{s2:<10}')
+print(f'{s3:<10}')
+print(f'{s4:<10}')
+print(f'{s1:^10}')
+print(f'{s2:^10}')
+print(f'{s3:^10}')
+print(f'{s4:^10}')
+print(f'{s1:>10}')
+print(f'{s2:>10}')
+print(f'{s3:>10}')
+print(f'{s4:>10}')
+
+# We finished session 03
