0acade4f269207d162c2ea6d310ca2603a7559c9
Zhi Li <lzblack@users.noreply.github.com>
2022-01-25 18:35:42 -0500

    Create hello.py

---
+++ session03/hello.py
@@ -0,0 +1,29 @@
+# print('Hey Jude', 'don\'t make it bad')
+
+# print('Number is', 39 + 41 + 33)
+
+# name = input('Please enter your name>>> ')
+# print('Hello, ' + name + '!')
+# print(f'Hello, {name}!')
+
+# a = 100
+# a + 200 # this is an expression not a statement
+
+# print('Naah, na na nanana naah, nanana naah, hey Jude.\n' * 10)
+
+pi = 3.1415926
+
+print(f'Pi equals {pi:.5f}.')
+print(f'Pi equals {pi:8.5f}.')
+print(f'Pi equals {pi:8.2f}.')
+
+s1 = 'a'
+s2 = 'ab'
+s3 = 'abc'
+s4 = 'abcd'
+
+print(f'{s1:>10}')
+print(f'{s2:>10}')
+print(f'{s3:>10}')
+print(f'{s4:>10}')
+print(f'{s4:^10}')
