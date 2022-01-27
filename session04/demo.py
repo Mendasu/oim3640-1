bc83328a5a3565a9d4f8a887ecfe6fc077e08b0f
Zhi Li <lzblack@users.noreply.github.com>
2022-01-27 18:24:23 -0500

    Finish session 04

---
+++ session04/demo.py
@@ -0,0 +1,40 @@
+import math
+import random
+
+
+# print(math.pi)
+# print(random.random())
+# print(random.random())
+# print(random.random())
+# print(random.random())
+
+
+# print('I\'m ok.')
+# print("I\'m \"ok\".")
+# print('I\'m learning\nPython.')
+# print('\\\n\\')
+# print(r'\\\n\\')
+# # print(r'\ \') # TODO: Figure this out
+# print(r'C:\user\documents')
+
+# print('''Multiple
+# line
+# another line''')
+
+
+# rewrite the next line to ask user to enter their age
+# in_China = input('Are you in China? >>> ')
+# age = input('How old are you? >>> ')
+# age = float(age)
+
+# if age >= 21 or in_China == 'yes':
+#     print('yes, you can.')
+# else:
+#     print('no, you cannot!')
+
+def f():
+    print('hi')
+    return 123
+
+result = f()
+print(result)
---
+++ session04/demo_01.py
@@ -0,0 +1,40 @@
+import math
+import random
+
+# print(3.14e-2)
+# print(math.pi)
+# print(random.random())
+# print(random.random())
+# print(random.random())
+
+# print('I\'m ok')
+# print('I\'m \"ok\".')
+# print('I\'m learning\nPython.')
+# print('\\\n\\')
+# print(r'\\\t\\')
+# print('''\\
+# \\''')
+# print('''Multiple
+# lines
+# last line''')
+# print(r'C:\user\documents')
+
+
+# Modify the next line so the program will ask user to enter their age
+# age = input('How old are you? >>> ')
+# # print(type(age))
+# age = int(age)
+# in_China = False
+# age = int(input('How old are you? >>> '))
+# if age > 21 or in_China:
+#     print('Yes, you can.')
+# else:
+#     print('No, you cannot.')
+
+
+def f():
+    print('hi')
+    return 123
+
+result = f()
+print(result)
