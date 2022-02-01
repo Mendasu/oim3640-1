5fd754da582cae28de21bcfd7c1ef52e04ca1e3b
Zhi Li <lzblack@users.noreply.github.com>
2022-02-01 18:31:57 -0500

    Create my_abs.py

---
+++ session05/my_abs.py
@@ -0,0 +1,18 @@
+def my_abs(number):
+    """print the absolute value of the given number"""
+    if number < 0:
+        print(-number)
+    else:
+        print(number)
+
+
+my_abs(-100)
+my_abs(100)
+
+
+def my_abs_2(number):
+    """return the absolute value for ex04"""
+
+
+def my_abs_3(number):
+    """for ex05"""
